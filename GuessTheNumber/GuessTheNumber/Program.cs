c1579d085d33808fd5c5abf1dc96f9b2a82c9363
viktoria.asboth <viktoria.asboth@SWS00498>
2017-01-18 16:26:45 +0100

    Added game logic.

--- GuessTheNumber/GuessTheNumber/Program.cs
+++ GuessTheNumber/GuessTheNumber/Program.cs
@@ -10,10 +10,40 @@ namespace GuessTheNumber
     {
         static void Main(string[] args)
         {
-            //int defaultNumber = EnterANumber("Please enter a number between 0 - 1000:");
+            int defaultNumber = EnterANumber("Please enter a number between 0 - 1000:");
 
-            //bool isInRange = IsNumberInRange(defaultNumber, 0, 1000);
+            bool isInRange = IsNumberInRange(defaultNumber, 0, 1000);
+            while (!isInRange)
+            {
+                Console.WriteLine("Number is not between 0 - 1000, please enter another number.");
+                defaultNumber = EnterANumber("Please enter a number between 0 - 1000:");
+                isInRange = IsNumberInRange(defaultNumber, 0, 1000);
+            }
 
+            Console.ReadLine();
+            Console.Clear();
+
+            int guessedNumber = EnterANumber("Please guess a number between 0 - 1000:");
+
+            isInRange = IsNumberInRange(guessedNumber, 0, 1000);
+            while (!isInRange)
+            {
+                Console.WriteLine("Number is not between 0 - 1000, please enter another number.");
+                guessedNumber = EnterANumber("Please enter a number between 0 - 1000:");
+                isInRange = IsNumberInRange(guessedNumber, 0, 1000);
+            }
+
+            bool isEqual = NumberEquals(defaultNumber, guessedNumber);
+            
+            while (!isEqual)
+            {
+                string message = guessedNumber > defaultNumber ? "smaller" : "greater";
+                Console.WriteLine("Incorrect. The number is {0}",message);
+                guessedNumber = EnterANumber("Please guess another number between 0 - 1000:");
+                isEqual = NumberEquals(defaultNumber, guessedNumber);
+            }
+            Console.WriteLine("Congtrats, you have guessed the correct number!");
+            Console.ReadLine();
 
         }
 
